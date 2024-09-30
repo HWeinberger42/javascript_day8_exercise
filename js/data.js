6d463800fb0b3cc47318f59b175663133273f676
Hermann Weinberger <miol@protonmail.com>
2024-09-30 11:52:12 +0200

    filled data items

---
+++ js/data.js
@@ -0,0 +1,69 @@
+let flowers = [ 
+    {
+        name: "Bearded Iris",
+        image: "bearded-iris-7537374_640.jpg",
+        // color = [white, blue, yellow],
+        price: 9.9,
+        amount: 1
+    },
+    {
+        name: "Daisy orange",
+        image: "daisy-orange-3978210_640.jpg",
+        price: 7.5,
+        amount: 1
+    },
+    {
+        name: "Gerbera",
+        image: "img/gerbera-7715379_640.jpg",
+        price: 14,
+        amount: 1
+    },
+    {
+        name: "Icelandic Poppy",
+        image: "iceland-poppy-8110_640.jpg",
+        price: 12.5,
+        amount: 1
+    },
+    {
+        name: "Lilly",
+        image: "lilies-7855265_640.jpg",
+        price: 10.6,
+        amount: 1
+    },
+    {
+        name: "Lilly Of The Valley",
+        image: "lily-of-the-valley-5117067_640.jpg",
+        price: 7.2,
+        amount: 1
+    },
+    {
+        name: "Orchid",
+        image: "orchid-50662_640.jpg",
+        price: 16.9,
+        amount: 1
+    },
+    {
+        name: "Poppy",
+        image: "poppy-5348626_640.jpg",
+        price: 5.2,
+        amount: 1
+    },
+    {
+        name: "Rose",
+        image: "rose-2811552_640.jpg",
+        price: 11.1,
+        amount: 1
+    },
+    {
+        name: "Sunflower",
+        image: "sunflower-7374363_640.jpg",
+        price: 24.5,
+        amount: 1
+    },
+    {
+        name: "Tulip",
+        image: "tulip-3333953_640.jpg",
+        price: 6.4,
+        amount: 1
+    }
+];
