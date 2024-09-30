69514a8d4804e79d8af79a74ad58c9aee64c64e8
Hermann Weinberger <miol@protonmail.com>
2024-09-30 15:35:43 +0200

    add id

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 let flowers = [ 
     {
+        id: crypto.randomUUID(),
         name: "Bearded Iris",
         image: "bearded-iris-7537374_640.jpg",
         // color = [white, blue, yellow],
@@ -7,60 +8,70 @@ let flowers = [
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Daisy orange",
         image: "daisy-orange-3978210_640.jpg",
         price: 7.5,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Gerbera",
         image: "gerbera-7715379_640.jpg",
         price: 14,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Icelandic Poppy",
         image: "iceland-poppy-8110_640.jpg",
         price: 12.5,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Lilly",
         image: "lilies-7855265_640.jpg",
         price: 10.6,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Lilly Of The Valley",
         image: "lily-of-the-valley-5117067_640.jpg",
         price: 7.2,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Orchid",
         image: "orchid-50662_640.jpg",
         price: 16.9,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Poppy",
         image: "poppy-5348626_640.jpg",
         price: 5.2,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Rose",
         image: "rose-2811552_640.jpg",
         price: 11.1,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Sunflower",
         image: "sunflower-7374363_640.jpg",
         price: 24.5,
         amount: 1
     },
     {
+        id: crypto.randomUUID(),
         name: "Tulip",
         image: "tulip-3333953_640.jpg",
         price: 6.4,
